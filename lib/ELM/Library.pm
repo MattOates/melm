827b6898c65856789b08dfa3048036b638956728
Matt Oates <mattoates@gmail.com>
2016-06-27 00:31:27 +0100

    Working ELM library OO interface

--- lib/ELM/Library.pm
+++ lib/ELM/Library.pm
@@ -1,58 +1,66 @@
-package ELM::Update 0.01;
+package ELM::Library 0.1;
 
-#Get a webpage as a string
-sub get_www {
-    my ($url) = @_;
+use v5.20.0;
+use strict;
+use warnings;
+use autodie;
+no warnings 'experimental::signatures';
+use feature 'signatures';
 
-    use LWP::UserAgent;
-    #Decided to localise this and just handle checking for deps properly
-    my $ua = LWP::UserAgent->new;
+use ELM::Utils 'get_www';
 
-    my $response = $ua->get($url);
-    if ($response->is_success) {
-       return $response->decoded_content;
-    } else {
-        die "Failed whilst contacting $url: " . $response->status_line;
+use Cwd qw/abs_path/;
+use Data::Dumper;
+#Make eval of dump simpler
+$Data::Dumper::Terse=1;
+
+use Class::Tiny qw/elms classes_version instances_version/, { 
+    elm_lib_path => sub {
+        $ENV{HOME} . '/.elm.dat';
     }
+};
+
+#Load the ELM classes when we instantiate
+sub BUILD($self, $args) {
+    $self->load_elm_classes;
 }
 
-#Upgrade the melm script to the latest version
-#TODO refactor this to get the latest release fatpack bundle!
-sub upgrade {
-    if (eval { require LWP::Protocol::https }) {
-        say STDERR "Upgrading melm version $VERSION to latest from GitHub.";
-        my $melm_path = abs_path(__FILE__);
-        say STDERR "Getting latest melm script from GitHub." if $verbose;
-        my $melm = get_www('https://raw.githubusercontent.com/MattOates/melm/master/bin/melm');
-        say STDERR "Upgrading your copy of melm to latest version." if $verbose;
-        open my $melm_fh, '>', $melm_path or die "Could not open melm script for writing.";
-        binmode($melm_fh, ':utf8');
-        print $melm_fh $melm;
-        close $melm_fh or die "Could not close melm script.";
-        `$melm_path --verbose --update`;
+#Check to see if the library exists
+sub exists($self) {
+    return -e $self->elm_lib_path;
+}
+
+#Load the cached ELM classes/instances data
+sub load_elm_classes($self) {
+    if ($self->exists) {
+        #Load the cached ELM classes/instances data
+        local $/; #slurp
+        open my $elm_fh, '<', $self->elm_lib_path;
+        my ($classes_version, $instances_version, $classes) =  @{ eval <$elm_fh> };
+        close $elm_fh;
+        $self->elms($classes);
+        $self->classes_version($classes_version);
+        $self->instances_version($instances_version);
     } else {
-        say STDERR 'Sorry GitHub requires HTTPS support and your Perl does not have LWP::Protocol::https installed.';
-        say STDERR 'Try `cpanm -i LWP::Protocol::https` to install this module.';
-        exit 1;
+        say STDERR "Could not find an ELM library so auto-fetching a fresh one for the first time";
+        $self->update;
     }
 }
 
 #Update the ELM definitions
-sub update {
-    if (eval { require LWP::UserAgent }) {
-        say STDERR "Updating ELM classes file $ENV{HOME}/.elm.dat" if $verbose;
-        update_elm_classes();
-        update_elm_instances();
-    } else {
-        say STDERR "On first execution and when updating melm requires your Perl installation to have LWP::UserAgent to contact ELM servers for data.";
-        say STDERR "You might be able to install this module using `cpanm -i LWP::UserAgent`.";
-        say STDERR "Another alternative is to download the latest ELM data from the melm repository at: https://raw.githubusercontent.com/MattOates/melm/master/.elm.dat";
-        say STDERR "Place the .elm.dat file into your home directory: /Users/username (OSX), /home/username (Linux) or C:\\Users\\username (Windows)"
-    }
+sub update($self) {
+    say STDERR "Updating ELM classes file " . $self->elm_lib_path;
+    $self->_update_elm_classes();
+    $self->_update_elm_instances();
+    open my $elm_fh,'>', $self->elm_lib_path;
+    say $elm_fh Dumper([$self->classes_version, $self->instances_version, $self->elms]);
+    close $elm_fh;
 }
 
 #Refresh the cached ELM classes file and populate %elms with latest data
-sub update_elm_classes {
+sub _update_elm_classes($self) {
+    my $classes_version;
+    $self->elms({}); #Wipe out existing elms
     my $class_tsv = get_www('http://elm.eu.org/elms/elms_index.tsv');
     #Each record looks like:
     #Pre 1.4
@@ -66,17 +74,15 @@ sub update_elm_classes {
         $record =~ s/"//g;
         my ($elm_id,$elm_name,$name,$description,$regex,$probability,$instances,$instances_in_pdb) = split /\t/, $record;
         my ($type) = split /_/, $elm_name;
-        $elms{$elm_name} = {accession => $elm_id, elm_name => $elm_name, site => $name, type => $type, description => $description, regex => $regex, probability => $probability};
+        $self->elms->{$elm_name} = {accession => $elm_id, elm_name => $elm_name, site => $name, type => $type, description => $description, regex => $regex, probability => $probability};
     }
-    open my $elm_fh,'>',"$ENV{HOME}/.elm.dat";
-    say $elm_fh Dumper([$classes_version,$instances_version,\%elms]);
-    close $elm_fh;
-    say STDERR "Updated ELM library to version $classes_version" if $verbose;
+    $self->classes_version($classes_version);
+    say STDERR "Updated ELM library to version $classes_version";
 }
 
 #Get the sequence for an instance from ELM
-sub get_instance_seqs {
-    say STDERR "Downloading instance protein sequences from ELM." if $verbose;
+sub _get_instance_seqs($self) {
+    say STDERR "Downloading instance protein sequences from ELM.";
     my $fasta = get_www("http://elm.eu.org/instances.fasta");
     my %sequences;
     #Hard assumption, ELM gives out sequences as a single line (appears true)
@@ -90,25 +96,24 @@ sub get_instance_seqs {
 }
 
 #Refresh the cached ELM instances data
-sub update_elm_instances {
+sub _update_elm_instances($self) {
+    my $instances_version;
     my %instance_logic = ('false positive' => 'FP','true negative' => 'TN','true positive', => 'TP', 'unknown' => 'U');
     my $instances_tsv = get_www('http://elm.eu.org/instances.tsv');
-    my %uniprot_sequences = get_instance_seqs();
+    my %uniprot_sequences = $self->_get_instance_seqs();
     #Each record looks like:
     #Accession, ELMType, ELMIdentifier, ProteinName, Primary_Acc, Accessions, Start, End, References, Methods, InstanceLogic, PDB, Organism
     #"ELMI000471"   "LIG"   "LIG_SH2_SRC"   "SRC_HUMAN" "P12931"    "P12931 E1P5V4 Q76P87 Q86VB9 Q9H5A8"    "530"   "533"   "10360179"  "x-ray crystallography" "true positive" ""  "Homo sapiens"
     foreach my $record (split /\n/, $instances_tsv) {
-        (undef,$instances_version) = split /: /, $record if ($record =~ /^#ELM_Instance_Download_Version/);
+        (undef, $instances_version) = split /: /, $record if ($record =~ /^#ELM_Instance_Download_Version/);
         next if $record =~ /^(#|"Accession)/; #Ignore the header
         $record =~ s/"//g;
         my ($elm_id, $type, $elm_name, $protein_name, $up_id, $alt_up_id, $start, $end, $references, $methods, $logic, $pdb, $organism) = split /\t/, $record;
         my $seq = substr( $uniprot_sequences{$up_id}, $start-1, 1+$end-$start );
-        push @{$elms{$elm_name}{instances}}, {accession => $elm_id, id => $up_id, start => $start, end => $end, logic => $instance_logic{$logic}, seq => $seq};
+        push @{$self->elms->{$elm_name}{instances}}, {accession => $elm_id, id => $up_id, start => $start, end => $end, logic => $instance_logic{$logic}, seq => $seq};
     }
-    say STDERR "Instances data updated to version $instances_version." if $verbose;
-    open my $elm_fh,'>',"$ENV{HOME}/.elm.dat";
-    say $elm_fh Dumper([$classes_version,$instances_version,\%elms]);
-    close $elm_fh;
+    say STDERR "Instances data updated to version $instances_version.";
+    $self->instances_version($instances_version);
 }
 
 1;
